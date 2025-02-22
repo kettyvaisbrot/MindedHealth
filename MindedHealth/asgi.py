2eaf81cdd949456d2d3292a3ae274510558da2dc
kettyvaisbrot <kettyvaisbrot@gmail.com>
2025-02-22 20:54:12 +0200

    Updated code and chane name to MindedHealth

--- MindedHealth/asgi.py
+++ MindedHealth/asgi.py
@@ -3,9 +3,9 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from WellMinded import routing  # Import the routing file
+from MindedHealth import routing  # Import the routing file
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WellMinded.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MindedHealth.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
