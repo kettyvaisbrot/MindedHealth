3b8f02304540e247318c68c20db292d5268d6f31
kettyvaisbrot <kettyvaisbrot@gmail.com>
2025-04-28 16:06:22 +0300

    add swagger

---
+++ chatbot/urls.py
@@ -0,0 +1,9 @@
+# chatbot/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/chat/', views.chatbot_response, name='chatbot_response'),  # API endpoint for chatbot interaction
+    path('', views.chatbot_page, name='chatbot_page'),  # Add this line
+
+]
