a6c43ba1f3f864f9fba2fb92c980598ba3f64321
kettyvaisbrot <kettyvaisbrot@gmail.com>
2025-06-09 18:36:10 +0300

    Add STATIC_ROOT for collectstatic

--- MindedHealth/settings.py
+++ MindedHealth/settings.py
@@ -17,6 +17,7 @@ import os
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
