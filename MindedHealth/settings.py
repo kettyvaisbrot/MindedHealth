5e61e834c464d8e56ff15e2dd12b6a5f32d0894c
kettyvaisbrot <kettyvaisbrot@gmail.com>
2025-12-14 20:45:40 +0200

    Standardize environment variable configuration

--- MindedHealth/settings.py
+++ MindedHealth/settings.py
@@ -46,15 +46,12 @@ SECURE_HSTS_PRELOAD = False
 
 # X-Frame-Options can stay for basic clickjacking protection
 X_FRAME_OPTIONS = 'DENY'
-
-from decouple import config
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = f"MindedHealth <{EMAIL_HOST_USER}>"
 
 # =======================
